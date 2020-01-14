ad26a0229e3e569d86e855ba0424bc140cbc6ceb
Волобуев Иван Юрьевич <ivolobuev@phoenixit.ru>
2020-01-14 13:02:06 +0300

    fix destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  def destro
+  def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
