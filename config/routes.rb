0adf658738c038cdd8804b7b1b470047ce028694
Волобуев Иван Юрьевич <ivolobuev@phoenixit.ru>
2020-01-24 11:29:41 +0300

    add devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
   resources :articles do
@@ -10,5 +11,5 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'welcome#index'
+  root to: 'welcome#index'
 end
